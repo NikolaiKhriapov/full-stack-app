38d43c3a4fd25278ac7851895d482a3f22a8d523
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-05-12 01:57:14 +0800

    Initial commit

---
+++ src/test/java/my/project/fullstackapp/FullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.fullstackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
