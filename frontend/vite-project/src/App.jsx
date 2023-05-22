97a4922a8560daa57fa7f75d62c8a74e1f94c3af
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-05-22 21:10:44 +0800

    1. Set up the frontend.

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+    const brand = "Full-Stack App"
+    return (
+        <div>
+            <h1>{brand}</h1>
+            <ol>
+                <li>React</li>
+                <li>React</li>
+                <li>React</li>
+                <li>React</li>
+            </ol>
+        </div>
+    )
+}
+
+export default App
