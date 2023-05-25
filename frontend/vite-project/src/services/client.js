70c81174ce297434b05c488b63a2abcee6afb253
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-05-25 23:52:09 +0800

    1. Finished CRUD for Customer; 2. Modified Card; 3. Added Docker for the frontend; 4. Started implementing security.

--- frontend/vite-project/src/services/client.js
+++ frontend/vite-project/src/services/client.js
@@ -1,8 +1,38 @@
 import axios from "axios";
 
-export  const getCustomers = async () => {
+export const getCustomers = async () => {
     try {
         return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`)
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const saveCustomer = async (customer) => {
+    try {
+        return await axios.post(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+            customer
+        )
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateCustomer = async (customerId, updatedCustomer) => {
+    try {
+        return await axios.put(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${customerId}`,
+            updatedCustomer
+        )
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteCustomer = async (customerId) => {
+    try {
+        return await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${customerId}`)
     } catch (error) {
         throw error;
     }
